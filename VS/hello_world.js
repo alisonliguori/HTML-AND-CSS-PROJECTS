b57c5fbc4f67ed575ff7952d1b359d89c7ea4ae0
Alison Liguori <alisonliguori0@gmail.com>
2025-09-21 12:37:46 -0400

    updated Petshopwebsite.  added to CSS file

---
+++ VS/hello_world.js
@@ -0,0 +1,9 @@
+var greeting = "Hello, World!";
+console.log(greeting);console.log("greeting");
+
+var greeting = "Welcome to Node.js!";
+console.log(greeting);console.log("greeting");
+
+var sentence1= "Learning how to debug code with the debugger";
+console.log(sentence1);
+
